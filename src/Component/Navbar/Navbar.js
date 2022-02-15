99176272aca0c92c1c9b0dafaddf0ed39254b696
prabhashrai02 <prabhashsahaj@gmail.com>
2022-02-15 19:35:58 +0530

    designed navbar

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,8 +1,17 @@
 import './Navbar.css';
+import bank_logo from '../../Assets/bank_logo.svg';
 
 const Navbar = () => {
     return (
         <div className="Navbar">
+            <div className="Navbar_bank">
+                <img src={bank_logo} alt="bank logo" />
+                <div className="Navbar_bank_name"><h2>Indian Bank</h2></div>
+            </div>
+            <div className="Navbar_options">
+                <button className="Navbar_add"><h3>Add Customer</h3></button>
+                <div className="Navbar_manager"><h3>Hi Admin</h3></div>
+            </div>
         </div>
     )
 }
