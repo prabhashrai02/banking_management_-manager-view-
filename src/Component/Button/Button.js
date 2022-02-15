7e7f3271e3c6abc659aec0a5a961894d6f72613e
prabhashrai02 <prabhashsahaj@gmail.com>
2022-02-15 20:19:55 +0530

    Added button component

---
+++ src/Component/Button/Button.js
@@ -0,0 +1,12 @@
+import './Button.css';
+
+const Button = (props) => {
+    return (
+        <button className={props.className}
+        type={props.type} onClick={props.onClick}>
+            {props.children}
+        </button>
+    )
+}
+
+export default Button;
--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,15 +1,18 @@
 import './Navbar.css';
 import bank_logo from '../../Assets/bank_logo.svg';
+import Button from '../Button/Button';
 
 const Navbar = () => {
     return (
         <div className="Navbar">
             <div className="Navbar_bank">
-                <img src={bank_logo} alt="bank logo" />
+                <img src={bank_logo} alt="bank logo" width ="70px"/>
                 <div className="Navbar_bank_name"><h2>Indian Bank</h2></div>
             </div>
             <div className="Navbar_options">
-                <button className="Navbar_add"><h3>Add Customer</h3></button>
+                <Button className="Navbar_add">
+                Add Customer
+                </Button>
                 <div className="Navbar_manager"><h3>Hi Admin</h3></div>
             </div>
         </div>
